5090aaae20e3752aaa736275f73de37074ccd617
XAmi101 <famousvacancy@gmail.com>
2019-08-08 11:04:25 -0400

    Components I MVP Doned

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -9,7 +9,7 @@ let menuItems = [
   'Log Out'
 ];
 
-/* 
+/*
 
   Step 1: Write a function that will create a menu component as seen below:
 
@@ -21,15 +21,36 @@ let menuItems = [
 
   Pass the function an array as it's only argument.
 
-  Step 2: Iterate over the array creating a list item <li> element for each item in the array. 
+  Step 2: Iterate over the array creating a list item <li> element for each item in the array.
   Add those items to the <ul>
 
   Step 3: Using a DOM selector, select the menu button currently on the DOM.
-  
+
   Step 4: add a click handler to the menu button, when clicked it should toggle the class 'menu--open' on the menu itself
 
   Step 5: return the menu component.
 
   Step 6: add the menu component to the DOM.
-  
+
 */
+const menuButton = document.querySelector('.menu-button')
+const header = document.querySelector('.header')
+ header.appendChild(menuCreator(menuItems))
+
+ function menuCreator(items){
+  const menu = document.createElement('div')
+  const list = document.createElement('ul')
+
+   menu.classList.add('menu')
+   menuItems.forEach(item => {
+    const listItems = document.createElement('li')
+    listItems.textContent = item
+    list.appendChild(listItems)
+     menu.appendChild(list)
+  })
+   menuButton.addEventListener('click', e => {
+    menu.classList.toggle('menu--open')
+   })
+
+   return menu
+}
